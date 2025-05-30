1d127331d0c2e64acdbd943c1780966de7dd702d
Antonio Musolino <antoniomusolino007@gmail.com>
2025-05-30 10:16:25 +0200

    chore: updates

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,12 +4,14 @@ import MainContent from "../components/MainContent";
 // src/pages/Main.tsx
 
 export default function Main() {
-    return (
-        <div lang="en">
-            {/* Head content va gestito con react-helmet o simili, qui lo omettiamo */}
-            <Sidebar />
-            // TODO: aggiungere il main content
-            <MainContent />
-        </div>
-    );
+  return (
+    <main>
+      <div lang="en">
+        {/* Head content va gestito con react-helmet o simili, qui lo omettiamo */}
+        <Sidebar />
+        // TODO: aggiungere il main content
+        <MainContent />
+      </div>
+    </main>
+  );
 }

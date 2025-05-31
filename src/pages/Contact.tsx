0fbcb7be7996870d6032132610c6695616a28bc1
Ebubechukwu Agbata <agbataebubechukwu@gmail.com>
2025-05-31 18:20:13 +0200

    feat: added pdfs

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
       <section className="mapbox" data-mapbox>
         <figure>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35620.797263497385!2d11.308603412407637!3d44.49777407560296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477fd498e951c40b%3A0xa2e17c015ba49441!2sBologna%20BO!5e0!3m2!1sit!2sit!4v1748699621980!5m2!1sit!2sit"
             width="400"
             height="300"
             loading="lazy"
